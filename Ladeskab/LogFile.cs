b61a760847e818b9b09993809cf9769bf98f256c
miningmanna <rasmus.carlsen@web.de>
2020-03-11 15:48:33 +0100

    Changed namespace for ILogFile and LogFile

--- Ladeskab/LogFile.cs
+++ Ladeskab/LogFile.cs
@@ -4,30 +4,33 @@ using System.IO;
 /// <summary>
 /// Summary description for Class1
 /// </summary>
-public class LogFile
+namespace Ladeskab
 {
+	public class LogFile : ILogFile
+	{
 
-	private string logFile = "logfile.txt";
+		private string logFile = "logfile.txt";
 
-	public LogFile();
-	public logWrite(int arg, int rfid)
-    {
-		using (var writer = File.AppendText(logFile))
-        {
-            if(arg = 1)
-            {
-                writer.WriteLine(DateTime.Now + ": Skab låst med RFID: {0}", rfid);
-            }
-            else if(arg = 2)
-            {
-                writer.WriteLine(DateTime.Now + ": Skab låst op med RFID: {0}", rfid);
-            }
-        }
-    }
+		public LogFile();
+		public logWrite(int arg, int rfid)
+		{
+			using (var writer = File.AppendText(logFile))
+			{
+				if(arg = 1)
+				{
+					writer.WriteLine(DateTime.Now + ": Skab låst med RFID: {0}", rfid);
+				}
+				else if(arg = 2)
+				{
+					writer.WriteLine(DateTime.Now + ": Skab låst op med RFID: {0}", rfid);
+				}
+			}
+		}
 
-}
+	}
 
-public interface ILogFile
-{
-    public logWrite(int arg, int rfid);
+	public interface ILogFile
+	{
+		public logWrite(int arg, int rfid);
+	}
 }
