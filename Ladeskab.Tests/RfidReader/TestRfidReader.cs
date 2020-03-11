b1a622c0bacfd49e75f9889b9694d198452626ed
miningmanna <rasmus.carlsen@web.de>
2020-03-11 13:46:25 +0100

    Added test for RfidDetected event

--- Ladeskab.Tests/RfidReader/TestRfidReader.cs
+++ Ladeskab.Tests/RfidReader/TestRfidReader.cs
@@ -15,6 +15,18 @@ namespace Ladeskab.Tests
 			_uut = new RfidReader();
 		}
 		
-		
+		[TestCase(123)]
+		[TestCase(2819923)]
+		[TestCase(0)]
+		public void TestRfidDetectedEvent(int id)
+		{
+			int arg = -1;
+			_uut.RfidDetected += (o, args) =>
+			{
+				arg = args.Id;
+			};
+			_uut.OnRfidRead(id);
+			Assert.AreEqual(arg, id);
+		}
 	}
 }
