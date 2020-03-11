89971be96236779234dd9edc1652911fb9684d6d
miningmanna <rasmus.carlsen@web.de>
2020-03-11 13:46:24 +0100

    Created folder for files concerning UsbCharger. Moved RfidDetectedEventArgs into IRfidReader.cs

--- Ladeskab/Rfid/IRfidReader.cs
+++ Ladeskab/Rfid/IRfidReader.cs
@@ -9,4 +9,13 @@ namespace Ladeskab
 		public event EventHandler<RfidDetectedEventArgs> RfidDetected;
 		
 	}
+	
+	public class RfidDetectedEventArgs : EventArgs
+	{
+		public int Id { get; }
+		public RfidDetectedEventArgs(int Id)
+		{
+			this.Id = Id;
+		}
+	}
 }
