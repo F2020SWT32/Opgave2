feff1ebd1ac006e2a7d018f6b010713499675a20
xKatze <hejhjepower@gmail.com>
2020-03-11 14:04:47 +0100

    first upload

---
+++ Ladeskab/Display.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Ladeskab
+{
+    public class Display
+    {
+        public string Msg { get; set; }
+        
+        public void ShowMsg()
+        {
+            Console.WriteLine(this.Msg);
+        }
+
+    }
+
+    /*
+    public class DisplayEventArgs : EventArgs
+    {
+        public Display Display { get; set; }
+    }
+    public class DisplayService
+    {
+        public EventHandler<DisplayEventArgs> DisplayShown;
+
+        public void DisplayMessage(Display message)
+        {
+            Console.WriteLine(message.Msg);
+
+            OnDisplayShown(message);
+        }
+
+        protected virtual void OnDisplayShown(Display message)
+        {
+            DisplayShown?.Invoke(this, new DisplayEventArgs() { Display = message });
+        }
+    }
+    */
+}
